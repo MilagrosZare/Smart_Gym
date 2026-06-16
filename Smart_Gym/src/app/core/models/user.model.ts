38a2535ba3ae3889170e5e2e60824d3e257f26a5
Milagros <miluurosario13@gmail.com>
2026-06-16 19:18:14 -0300

    Construcción del login sin funcionalidad

--- Smart_Gym/src/app/core/models/user.model.ts
+++ Smart_Gym/src/app/core/models/user.model.ts
@@ -6,3 +6,14 @@ export interface User {
     rol: 'SOCIO' | 'ADMIN';
     telefono: number;
 }
+
+
+export interface LoginRequest { /* front envía a back */
+    email: string;
+    password: string;
+}
+
+export interface LoginResponse {  /* back devuelve a front si el login fue exitoso */
+    token: string; 
+    usuario: User;
+}
